2654139e1a0852674febca9b3cb84209390eb6af
eytsugey <eytsugey@gmail.com>
2026-02-27 08:43:27 -0700

    Update logout redirection to login.php

--- public/logout.php
+++ public/logout.php
@@ -1,4 +1,11 @@
 <?php
 session_start();
+
+$_SESSION = [];
+
+// Destruir sesión
 session_destroy();
-header("Location: login.html");
+
+// Redirigir al login
+header("Location: login.php");
+exit;
